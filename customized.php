8d8926f2540dc7371f01614c9dc0a3e6c70de304
Your Name Here <bartakke7@gmail.com>
2016-12-20 22:45:47 +0530

    creative-design 20 dec

--- customized.php
+++ customized.php
@@ -15,7 +15,7 @@
 	<div class="row " >
 		<div class="col-sm-3 noPadding ">
 			<div class="card">
-				<div class="bs-example paddingtop20 padding10  " style="min-height:600px">
+				<div class="bs-example  padding10  " id="thumbwrap" style="min-height:600px">
 				    <div class="panel-group" id="accordion">
 				        <div class="panel panel-default">
 				            <div class="panel-heading">
@@ -24,22 +24,54 @@
 				                </h4>
 				            </div>
 				            <div id="collapseOne" class="panel-collapse collapse in">
-				                <div class="panel-body">
+				                <div class="panel-body" >
 				                    <ul>
 				                    	<li>
-				                    		<a href="#" class="namplateName">Fernandes H....Image preview</a>
-				                    		<div class="namePlateImg">
-												<img src="http://placehold.it/170x230">
-											</div>
+				                    		<a class="thumb" href="#">
+												Fernandes H....Image preview
+												<span><img src="images/product1.jpg" class="img-responsive" alt=""></span>
+											</a>
 				                    	</li>
-										<li><a href="#">Nair Villa</a></li>	
-										<li><a href="#">Seths Residence</a></li>
-										<li><a href="#">V K Reddy</a></li>
-										<li><a href="#">13 JVPD</a></li>
-										<li><a href="#">Patnayaks</a></li>
-										<li><a href="#">Shettys</a></li>
-										<li><a href="#">Designer Charlet</a></li>
-										<li><a href="#">Jains</a></li>	
+										<li>
+											<a  class="thumb"href="#">Nair Villa
+												<span><img src="images/product2.jpg" class="img-responsive" alt=""></span>
+											</a>	
+										</li>	
+										<li>
+											<a class="thumb" href="#">Seths Residence
+												<span><img src="images/product3.jpg" class="img-responsive" alt=""></span>
+											</a>
+										</li>
+										<li>
+											<a class="thumb" href="#">V K Reddy
+												<span><img src="images/product4.jpg" class="img-responsive" alt=""></span>
+											</a>	
+										</li>
+										<li>
+											<a class="thumb" href="#">13 JVPD
+												<span><img src="images/product5.jpg" class="img-responsive" alt=""></span>
+											</a>
+										</li>
+										<li>
+											<a class="thumb" href="#">Patnayaks
+												<span><img src="images/product6.jpg" class="img-responsive" alt=""></span>
+											</a>	
+										</li>
+										<li>
+											<a  class="thumb"href="#">Shettys
+												<span><img src="images/product7.jpg" class="img-responsive" alt=""></span>
+											</a>	
+										</li>
+										<li>
+											<a class="thumb" href="#">Designer Charlet
+												<span><img src="images/product8.jpg" class="img-responsive" alt=""></span>
+											</a>
+										</li>
+										<li>
+											<a class="thumb" href="#">Jains
+												<span><img src="images/product9.jpg" class="img-responsive" alt=""></span>
+											</a>
+										</li>	
 				                    </ul>
 				                </div>
 				            </div>
@@ -138,7 +170,10 @@
 		</div>
 
 		<div class="col-sm-6 ">
-
+			<div class="col-sm-12">
+				<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+				<img id="drag1" src="images/showCase17.jpg" draggable="true" ondragstart="drag(event)" width="336" height="69">
+			</div>	
 		</div>
 
 
@@ -148,12 +183,12 @@
 					<h4>Your Name as on Name Plate</h4>
 				</div>
 				<div class="row">
-					<div class="col-sm-4 inputWidth1 paddingright20 ">
+					<div class="col-sm-6 inputWidth1  ">
 						<h5>Line 1	</h5>
 						<input type="text" class="form-control inputSize ">
 					</div>
-					<div class="col-sm-8  inputWidth paddingright20 noPadding ">
-						<div class="col-sm-4">
+					<div class="col-sm-6  inputWidth paddingright20 noPadding ">
+						<div class="col-sm-6">
 							<h5>Size</h5>
 							
 				        	<div class="bs-docs-example custSize">
@@ -180,22 +215,27 @@
 				              </select>
 			              </div>
 						</div>	
-						<div class="col-sm-4">	
-							<h5>Angle</h5>	
+						<!-- <div class="col-sm-4">	
+							<h5>Font</h5>	
 							<div class="bs-docs-example custSize">
 					              <select class="selectpicker" data-style="btn-primary">
-					                  	<option>Yes</option>
-							            <option>No</option>
+					                  	<option style="font-family: 'Comic Sans MS'">Comic Sans MS</option>
+     									<option style="font-family: Arial">Arial</option>
+     									<option style="font-family: 'Times New Roman'">Times New Roman</option>
 							        </select>
 							    </div>     
-						</div>
-						<div class="col-sm-4">	
-							<h5>Bold</h5>
+						</div> -->
+						<div class="col-sm-6">	
+							<h5>Color</h5>
 							<!-- <input type="text"> -->
-							<div class="bs-docs-example custSize">
+							<div class="bs-docs-example custSize" >
 					              <select class="selectpicker" data-style="btn-primary">
-					                  	<option>Yes</option>
-							            <option>No</option>
+					                  	<option class=" height30" value="1" style="background-color: blue"></option>
+  										<option value="2" style="background-color: green"></option>
+  										<option value="2" style="background-color: red"></option>
+  										<option value="2" style="background-color: black"></option>
+  										<option value="2" style="background-color: white"></option>
+							            
 							        </select>
 							    </div>     
 						</div>
@@ -204,14 +244,15 @@
 				<hr class="hrLine">
 
 				<div class="row">
-					<div class="col-sm-4 inputWidth1 ">
-						<h5>Line 2	</h5>
+					<div class="col-sm-6 inputWidth1  ">
+						<h5>Line 1	</h5>
 						<input type="text" class="form-control inputSize ">
 					</div>
-					<div class="col-sm-8  inputWidth paddingright20 noPadding ">
-						<div class="col-sm-4">
+					<div class="col-sm-6  inputWidth paddingright20 noPadding ">
+						<div class="col-sm-6">
 							<h5>Size</h5>
-							<div class="bs-docs-example custSize">
+							
+				        	<div class="bs-docs-example custSize">
 				              <select class="selectpicker" data-style="btn-primary">
 				                  	<option>10</option>
 						            <option>20</option>
@@ -235,21 +276,27 @@
 				              </select>
 			              </div>
 						</div>	
-						<div class="col-sm-4">	
-							<h5>Angle</h5>	
-								<div class="bs-docs-example custSize">
-					              <select class="selectpicker" data-style="btn-primary">
-					                  	<option>Yes</option>
-							            <option>No</option>
-							        </select>
-							    </div>        
-						</div>
-						<div class="col-sm-4">	
-							<h5>Bold</h5>
+						<!-- <div class="col-sm-4">	
+							<h5>Font</h5>	
 							<div class="bs-docs-example custSize">
 					              <select class="selectpicker" data-style="btn-primary">
-					                  	<option>Yes</option>
-							            <option>No</option>
+					                  	<option style="font-family: 'Comic Sans MS'">Comic Sans MS</option>
+     									<option style="font-family: Arial">Arial</option>
+     									<option style="font-family: 'Times New Roman'">Times New Roman</option>
+							        </select>
+							    </div>     
+						</div> -->
+						<div class="col-sm-6">	
+							<h5>Color</h5>
+							<!-- <input type="text"> -->
+							<div class="bs-docs-example custSize" >
+					              <select class="selectpicker" data-style="btn-primary">
+					                  	<option value="1" style="background-color: blue"></option>
+  										<option value="2" style="background-color: green"></option>
+  										<option value="2" style="background-color: red"></option>
+  										<option value="2" style="background-color: black"></option>
+  										<option value="2" style="background-color: white"></option>
+							            
 							        </select>
 							    </div>     
 						</div>
@@ -258,14 +305,15 @@
 				<hr class="hrLine">
 
 				<div class="row">
-					<div class="col-sm-4 inputWidth1 ">
-						<h5>Line 3	</h5>
+					<div class="col-sm-6 inputWidth1  ">
+						<h5>Line 1	</h5>
 						<input type="text" class="form-control inputSize ">
 					</div>
-					<div class="col-sm-8  inputWidth paddingright20 noPadding ">
-						<div class="col-sm-4">
+					<div class="col-sm-6  inputWidth paddingright20 noPadding ">
+						<div class="col-sm-6">
 							<h5>Size</h5>
-							<div class="bs-docs-example custSize">
+							
+				        	<div class="bs-docs-example custSize">
 				              <select class="selectpicker" data-style="btn-primary">
 				                  	<option>10</option>
 						            <option>20</option>
@@ -289,21 +337,27 @@
 				              </select>
 			              </div>
 						</div>	
-						<div class="col-sm-4">	
-							<h5>Angle</h5>	
+						<!-- <div class="col-sm-4">	
+							<h5>Font</h5>	
 							<div class="bs-docs-example custSize">
 					              <select class="selectpicker" data-style="btn-primary">
-					                  	<option>Yes</option>
-							            <option>No</option>
+					                  	<option style="font-family: 'Comic Sans MS'">Comic Sans MS</option>
+     									<option style="font-family: Arial">Arial</option>
+     									<option style="font-family: 'Times New Roman'">Times New Roman</option>
 							        </select>
 							    </div>     
-						</div>
-						<div class="col-sm-4">	
-							<h5>Bold</h5>
-							<div class="bs-docs-example custSize">
+						</div> -->
+						<div class="col-sm-6">	
+							<h5>Color</h5>
+							<!-- <input type="text"> -->
+							<div class="bs-docs-example custSize" >
 					              <select class="selectpicker" data-style="btn-primary">
-					                  	<option>Yes</option>
-							            <option>No</option>
+					                  	<option value="1" style="background-color: blue"></option>
+  										<option value="2" style="background-color: green"></option>
+  										<option value="2" style="background-color: red"></option>
+  										<option value="2" style="background-color: black"></option>
+  										<option value="2" style="background-color: white"></option>
+							            
 							        </select>
 							    </div>     
 						</div>
@@ -311,7 +365,7 @@
 				</div>
 				<hr class="hrLine">
 
-				<div class="row padding10 ">
+				<!-- <div class="row padding10 ">
 					<div class="CustomSubHead">
 						<h4>Your Name as on Name Plate</h4>
 					</div>
@@ -370,21 +424,46 @@
 							</div>	
 					</div>   
 				<hr class="hrLine">
-			</div>	
+			</div> -->	
 
-				<div class="CustomSubHead">
-					<h4>Font Family</h4>
-				</div>
-				<div class="col-sm-12  fillterNav margintop20">
-		    		<ul class="marginbottom30 ">
-				        <li class="dropdown">
-				          <a href="#" class="dropdown-toggle" data-toggle="dropdown"> 
-				          	<input type="text" class="form-control inputSize " style="width=200px;"id="usr"> </a>
-				          <ul class="dropdown-menu fillterNavMenu">
-				            <ul id="fnt" style="width: 189px; display: block;"><li style="font-family: Amsterdam, Amsterdam;">Amsterdam</li><li style="font-family: Athens, Athens;">Athens</li><li style="font-family: Baghdad, Baghdad;">Baghdad</li><li style="font-family: Baku, Baku;">Baku</li><li style="font-family: Bangkok, Bangkok;">Bangkok</li><li style="font-family: Beijing, Beijing;">Beijing</li><li style="font-family: Berlin, Berlin;">Berlin</li><li style="font-family: Bridgetown, Bridgetown;">Bridgetown</li><li style="font-family: Cairo, Cairo;">Cairo</li><li style="font-family: Canberra, Canberra;">Canberra</li><li style="font-family: Delhi, Delhi;">Delhi</li><li style="font-family: Doha, Doha;">Doha</li><li style="font-family: Dublin, Dublin;">Dublin</li><li style="font-family: Hanoi, Hanoi;">Hanoi</li><li style="font-family: Kingston, Kingston;">Kingston</li><li style="font-family: Lima, Lima;">Lima</li><li style="font-family: Lisbon, Lisbon;">Lisbon</li><li style="font-family: London, London;">London</li><li style="font-family: Madrid, Madrid;">Madrid</li><li style="font-family: Monaco, Monaco;">Monaco</li><li style="font-family: Moscow, Moscow;">Moscow</li><li style="font-family: Muscat, Muscat;">Muscat</li><li style="font-family: Oslo, Oslo;">Oslo</li><li style="font-family: Ottawa, Ottawa;">Ottawa</li><li style="font-family: Paris, Paris;">Paris</li><li style="font-family: Riga, Riga;">Riga</li><li style="font-family: Rome, Rome;">Rome</li><li style="font-family: Singapore, Singapore;">Singapore</li><li style="font-family: Sofia, Sofia;">Sofia</li><li style="font-family: Vienna, Vienna;">Vienna</li><li style="font-family: Washington, Washington;">Washington</li></ul>
-				          </ul>
-				        </li>
-		      		</ul>
+					<div class="CustomSubHead margintop20">
+						<h4>Font Family</h4>
+					</div>
+					<div class="  fillterNav margintop20">
+						<h5>Line 1	</h5>
+			    		<div class="bs-docs-example custSize1" >
+			              	<select class="selectpicker" data-style="btn-primary">
+			                  	<option class="specialColor" value="volvo">Volvo</option>
+								<option class="specialColor" value="saab">Saab</option>
+								<option class="specialColor" value="mercedes">Mercedes</option>
+								<option class="specialColor" value="audi">Audi</option>
+					        </select>
+					    </div>     
+					</div>
+
+					<div class="  fillterNav margintop20">
+						<h5>Line 2	</h5>
+			    		<div class="bs-docs-example custSize1" >
+			              	<select class="selectpicker" data-style="btn-primary">
+			                  	<option class="specialColor" value="volvo">Volvo</option>
+								<option class="specialColor" value="saab">Saab</option>
+								<option class="specialColor" value="mercedes">Mercedes</option>
+								<option class="specialColor" value="audi">Audi</option>
+					        </select>
+					    </div>     
+					</div>
+
+					<div class="  fillterNav margintop20">
+						<h5>Line 3	</h5>
+			    		<div class="bs-docs-example custSize1" >
+			              	<select class="selectpicker" data-style="btn-primary">
+			                  	<option class="specialColor" value="volvo">Volvo</option>
+								<option class="specialColor" value="saab">Saab</option>
+								<option class="specialColor" value="mercedes">Mercedes</option>
+								<option class="specialColor" value="audi">Audi</option>
+					        </select>
+					    </div>     
+					</div>
 			    </div>
 				<hr class="hrLine">
 				<div class="clearfix"></div>
@@ -607,18 +686,23 @@
 	</div>
 
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
 <? include "footer.php" ?>
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Status Help
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+   <script src="js/jquery-2.2.0.min.js" type="text/javascript"></script>
+    <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script-->
+  <script src="js/bootstrap.min.js"></script>
+<script>
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+</script>
